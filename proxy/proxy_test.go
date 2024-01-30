dbc3909b66034318f9334bc62dc301dd73a2df6c
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2024-01-30 14:37:00 -0300

    inspectors: simplify request authorization
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -133,9 +133,8 @@ func (t *proxySuite) TestProxyDownload(c *C) {
 	c.Assert(dl.ContentType, Equals, "application/x-debian-package")
 	c.Assert(dl.UserAgent, Equals, "Go-http-client/1.1")
 	c.Assert(dl.RequestHeader, DeepEquals, map[string][]string{
-		"Accept-Encoding":    []string{"gzip"},
-		"User-Agent":         []string{"Go-http-client/1.1"},
-		"X-Fetch-Session-Id": []string{"6ba7b8109dad11d180b400c04fd430c8"},
+		"Accept-Encoding": []string{"gzip"},
+		"User-Agent":      []string{"Go-http-client/1.1"},
 	})
 	c.Assert(dl.ResponseHeader["Content-Length"], DeepEquals, []string{"26600"})
 	c.Assert(dl.ResponseHeader["Content-Type"], DeepEquals, []string{"application/x-debian-package"})
